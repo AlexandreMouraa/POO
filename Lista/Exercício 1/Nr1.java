01bc3fb7d46b645cd30bb63fc8534dada0dca95c
AlexandreMouraa <alexandree.moouraa6@gmail.com>
2023-05-15 12:51:56 -0300

    Lista

---
+++ Lista/Exercício 1/Nr1.java
@@ -0,0 +1,7 @@
+public class Nr1
+{
+    public static void main(String [] args)
+    {
+        
+    }
+}
---
+++ Lista/Exercício 1/Nr2.java
@@ -0,0 +1,7 @@
+public class Nr2 
+{
+    public static void main(String [] args)
+    {
+        
+    }
+}

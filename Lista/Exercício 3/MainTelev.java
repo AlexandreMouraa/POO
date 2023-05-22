31e7fa02cfb3d0206165dfa1fccbe6c4161d6dbd
AlexandreMouraa <alexandree.moouraa6@gmail.com>
2023-05-22 12:45:00 -0300

    Exercício 3

--- Lista/Exercício 3/MainTelev.java
+++ Lista/Exercício 3/MainTelev.java
@@ -2,7 +2,54 @@ public class MainTelev
 {
     public static void main(String [] args)
     {
+        //Automaticamente começam com 0 no construtor.
         Televisao Tela1 = new Televisao();
+
+        //Mostrando Volume e Canal atual com a TV desligada
+        System.out.println(Tela1);
+
+        //Mostrando que o contador está funcionando
+        System.out.println("O contador do construtor é: " + Televisao.GetCont());
+        System.out.println();
+
+        //Mostrando que mesmo mudando de canal com ela desligada não haverá alteração
+        //ela só muda de canal ligada
+        Tela1.MudarCanal(3);
+        Tela1.AumentarVolume(25);
+
+        //Sua Situação Atual
+        System.out.println("Sua situação atual mudando o canal e volume com ela desligada é: (Não haverá mudança com ela desligada)");
+        System.out.println(Tela1);
+
+        //Ligando a TV
+        Tela1.LigarTV();
+        System.out.println();
+
+        //Situaçao atual
+        System.out.println("Sua situação atual depois de ligar ela sem alterar canais e volume:");
+        System.out.println(Tela1);
+        System.out.println();
+
+        //Alterando o canal e volume com ela ligada nesse momento
+        Tela1.MudarCanal(3);
+        Tela1.AumentarVolume(25);
+
+        //Sua Situação Atual
+        System.out.println("Sua situação atual depois de alterar canais e volume:");
+        System.out.println(Tela1);
+        System.out.println();
+
+        //Desligando a TV
+        Tela1.DesligarTV();
+
+        //Sua situação atual
+        System.out.println("Sua situação atual depois de desligar a TV:");
+        System.out.println(Tela1);
+        System.out.println();
+
+        //Mostrando o contador do construtor:
+        System.out.println("O contador do construtor é: " + Televisao.GetCont()); 
+        //Automaticamente quando "chamá-lo" novamente, o canal e volume estarão em Zero. Mesmo que no Desligar eles já tenha sido zerados
     }
     
 }
--- Lista/Exercício 3/Televisao.java
+++ Lista/Exercício 3/Televisao.java
@@ -3,6 +3,7 @@ public class Televisao
     private int Canal;
     private int volume;
     private static int cont = 0;
+    boolean Ligada = false;
 
     public Televisao()
     {
@@ -14,24 +15,54 @@ public class Televisao
         cont++;
     }
 
+    public boolean LigarTV()
+    {
+        if(cont > 0)
+            Ligada = true; 
+
+        return Ligada;
+    }
+
+    public boolean DesligarTV()
+    {
+        volume = 0;
+        Canal = 0;
+        cont = 0;
+        Ligada = false;
+
+        return Ligada;
+    }
+
     public void AumentarVolume(int Aumentar)
     {
-        volume += Aumentar;
+        if(Ligada)
+            volume += Aumentar;
     }
 
     public void DiminuirVolume(int Diminuir)
     {
-        volume -= Diminuir;
+        if(Ligada)
+            volume -= Diminuir;
     }
 
     public void MudarCanal(int Qual)
     {
-        Canal += Qual;
+        if(Ligada)
+            Canal += Qual;
+    }
+
+    public static int GetCont()
+    {
+        return cont;
     }
 
     @Override
     public String toString()
     {
-        return "O canal atual é: " + Canal + "\nO volume está em: " + volume;
+        if(Ligada)
+            return "A Tv está ligada " + "\nO canal atual é: " + Canal + "\nO volume está em: " + volume;
+
+        else
+            return "A Tv está desligada " + "\nO canal atual é: " + Canal + "\nO volume está em: " + volume;
     }
 }
